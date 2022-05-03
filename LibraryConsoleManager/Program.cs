98e0c662eeef22da84a63338769088e3def535ad
jarso1 <104627298+jarso1@users.noreply.github.com>
2022-05-03 22:43:28 +0200

    proste menu na do while

--- LibraryConsoleManager/Program.cs
+++ LibraryConsoleManager/Program.cs
@@ -10,6 +10,48 @@ namespace LibraryConsoleManager
     {
         static void Main(string[] args)
         {
+            int choice = 9;
+            do 
+            {
+                Console.WriteLine("Witamy w bibliotece. wybierz co chcesz zrobić:");
+                Console.WriteLine("1-dodaj książkę");
+                Console.WriteLine("2-dodaj płytę");
+                Console.WriteLine("3-dodaj czasopismo");
+                Console.WriteLine("4-usuń pozycję");
+                Console.WriteLine("5-wyświetl wszystkie pozycje");
+                Console.WriteLine("6-wyświetl wszystkie pozycje danego podtypu");
+                Console.WriteLine("0-wyjście");
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception) { Console.WriteLine("proszę podać cyfrę"); };
+                switch (choice)
+                {
+                    case 1:
+                        //addbook();
+                         break;
+                    case 2:
+                         //addCD();
+                         break;
+                    case 3:
+                        //addmagazine();
+                        break;
+                    case 4:
+                        //delete();
+                        break;
+                    case 5:
+                        //show-all();
+                        break;
+                    case 6:
+                        //show-specyfic-all();
+                        break;
+                    default:
+                        Console.WriteLine("proszę podać numer z listy");
+                        break;
+                }
+            }
+            while (choice!=0); 
         }
     }
 }
